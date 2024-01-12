3a801acf87df559a572e6fa3327687e8c97cebbd
Wallace Patrick <patrickwallace621@gmail.com>
2024-01-12 16:17:56 -0300

    Class field attributes

--- src/br/com/wallace/mf/model/Field.java
+++ src/br/com/wallace/mf/model/Field.java
@@ -1,5 +1,22 @@
 package br.com.wallace.mf.model;
 
-public class Field {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Field {
+	
+	private final int line;
+	private final int column;
+	
+	private boolean opened;
+	private boolean undermine;
+	private boolean marked;
+	
+	private List<Field> neighbors = new ArrayList<>();
+	
+	
+	Field (int line, int column) {
+		this.line = line;
+		this.column = column;
+	}
 }

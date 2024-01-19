7ed85654caab61077b3a882d2a10c3002c408f2d
Wallace Patrick <patrickwallace621@gmail.com>
2024-01-19 10:57:43 -0300

    Board class - (generateFields - associateNeighbors)

--- src/br/com/wallace/mf/model/Board.java
+++ src/br/com/wallace/mf/model/Board.java
@@ -1,5 +1,45 @@
 package br.com.wallace.mf.model;
 
-public class Board {
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
+public class Board {
+	private int lines;
+	private int columns;
+	private int mines;
+	
+	private final List<Field> fields = new ArrayList<>();
+
+	public Board(int lines, int columns, int mines) {
+		this.lines = lines;
+		this.columns = columns;
+		this.mines = mines;
+		
+		generateFields();
+		associateNeighbors();
+		drawMines();
+		
+	}
+
+	private void generateFields() {
+		for (int l = 0; l < lines; l++) {
+			for (int c = 0; c < columns; c++) {
+				fields.add(new Field(l, c));
+			}
+		}
+		
+	}
+	
+	private void associateNeighbors() {
+		for(Field f1 : fields) {
+			for(Field f2 : fields) {
+				f1.addNeighbors(f2);
+			}
+		}
+	}
+	
+	private void drawMines() {
+		
+	}
 }
